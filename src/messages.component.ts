421e5d1a1b545293ae6b602380a458d1b531dfaf
Octavio Garbarino <octaviogarbarino@gmail.com>
2016-12-23 15:58:56 -0300

    unused import removed

--- src/messages.component.ts
+++ src/messages.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { MessageService} from './message.service';
-import { Observable }     from 'rxjs/Observable';
 
 @Component({
     selector: 'messages-component',
